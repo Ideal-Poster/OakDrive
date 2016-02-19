2dac3ea059bf3da094cd056a8a7aaf8c9728aa34
Malcolm Gourdine <malcolmgourdine@Malcolms-MBP.home>
2016-02-18 22:27:59 -0500

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Episode.create!([
+  {title:"Grady Baby", description:"this is the first on", guests:"tom hanks", link:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/211644050&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"},
+  {title:"Poison", description:"anotha one", guests:"meadowlark lemon", link:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/211245201&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"},
+  {title:"You good?", description:"break out the soda and gummy worms", guests:"peewee herman", link:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/213166350&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"},
+  {title:"Do me", description:"crud!", guests:"wesley snipes", link:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/217956367&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"},
+  {title:"Bits and Pieces", description:"vortex episode. Welcome to the end of the world", guests:"jrue holiday", link:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/217956367&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"},
+  {title:"Regular Flags", description:"relationships and other stuff like that", guests:"the salt barron", link:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/239614750&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"}
+  ])
