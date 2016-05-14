337f7ee745cdae7cd4eaccb6cd24b102feac3223
Ideal-Poster <malcolmgourdine@gmail.com>
2016-05-14 14:10:29 -0400

    mobile search enabled. general style cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def cp(path)
     "current" if current_page?(path)
   end
+
+  def title_case(title, minor_words = '')
+    title.capitalize.split().map{|a| minor_words.downcase.split().include?(a) ? a : a.capitalize}.join(' ')
+  end
 end
