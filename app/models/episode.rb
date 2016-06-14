e3de382c37d963862de9ead67240f9a64516b053
Ideal-Poster <malcolmgourdine@gmail.com>
2016-06-14 13:56:08 -0400

    authorization added.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,15 +1,17 @@
 class Episode < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "180x180>" }
-  #validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   has_attached_file :mp3
   validates_attachment :mp3,
       :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] },
       :file_name => { :matches => [/mp3\Z/] }
-      def self.text_search(query)
-        if query.present?
+
+    def self.text_search(query)
+      if query.present?
           where("title @@ :q or description @@ :q or guests @@ :q", q: query)
-        end
       end
+    end
+
   belongs_to :user
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
+
   has_attached_file :image, styles: { medium: "300x300>", thumb: "240x240>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  belongs_to :user
 end
